723e284f8d9e2aa2936069a3f74777377f6886f4
vietvd13 <vuducviet0131@gmail.com>
2020-08-05 17:09:34 +0700

    Update UI for Quizzes + Search

--- src/components/Quizzes.vue
+++ src/components/Quizzes.vue
@@ -3,26 +3,28 @@
       <div class="row">
         <div class="col-sm">
           <div class="card" id="qoutes">
-            <div class="card-body">
-              <div class="media align-items-center">
-                <div class="media-body">
-                  <span>"Your time is limited, so don't waste it living someone else's life." <br> "Thời gian của bạn luôn có hạn, vì vậy đừng lãng phí nó để sống cuộc đời khác."</span>
-                  </div>
-                      <div class="text-center">
-                          <i class="icofont-education"></i>
-                      </div>
-                  </div>
+            <b-row>
+              <b-col>
+                <div id="title-code">
+                  <h4>Select Example Code</h4>
                 </div>
-            </div>
+                <b-row class="my-1">
+                  <b-col sm="12">
+                    <b-form-select v-model="optionSelected" :options="options"></b-form-select>
+                  </b-col>
+                </b-row>
+              </b-col>
+            </b-row>
+          </div>
         </div>
 
         <div class="col-sm">
           <div class="card search">
               <center>
-                <h4 id="title-search">Search Name Student</h4>
+                <h4 id="title-search">Search Student ID</h4>
                 <b-container class="bv-example-row">
                   <b-row>
-                    <b-col class="col-8"><b-form-input v-model="keyword" type="text" debounce="500" placeholder="Search Question" id="keyword-input"></b-form-input></b-col>
+                    <b-col class="col-8"><b-form-input v-model="keyword" type="text" debounce="500" placeholder="Search Student by ID" id="keyword-input"></b-form-input></b-col>
                     <b-col class="col-4"><b-button>Search</b-button></b-col>
                   </b-row>
                 </b-container>
@@ -37,18 +39,43 @@
             <h4>Quizzes</h4>
           </div>
           <div id="table-quizzes">
-            <b-table stacked="md" hover :items="items" :fields="fields">
-                <template v-slot:cell(Actions)>
-                  <input type="checkbox">
-              </template>
-            </b-table>
+            <table v-if="items.length" class="table table-hover">
+              <thead>
+                <tr>
+                  <th class="pick">
+                    <label>
+                      <input type="checkbox" v-model="selectAll" @click="select">
+                    </label>
+                  </th>
+                  <th>ID</th>
+                  <th>Sender ID</th>
+                  <th>Student ID</th>
+                  <th>Fullname</th>
+                  <th>Class</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="item in resultQuery" :key="item.No">
+                  <td>
+                    <label>
+                      <input type="checkbox" :value="item.No" v-model="selected">
+                    </label>
+                  </td>
+                  <td>{{ item.No }}</td>
+                  <td>{{ item.SenderID }}</td>
+                  <td>{{ item.StudentID }}</td>
+                  <td>{{ item.FullName }}</td>
+                  <td>{{ item.Class }}</td>
+                </tr>
+              </tbody>
+            </table>
+
           </div>
 
           <div class="submit">
             <div class="row justify-content-md-center">
               <div class="col-md-auto" id="btn-submit">
                 <b-button class="send">Send</b-button>
-                <b-button class="send-all">Send All Student</b-button>
               </div>
             </div>
           </div>
@@ -63,37 +90,12 @@ export default {
     data() {
       return {
         keyword: '',
-        fields: [
-          {
-            key: 'No',
-            sortable: true,
-            class: 'no-quizzes'
-          },
-          {
-            key: 'SenderID',
-            sortable: true,
-            class: 'senderid-quizzes'
-          },
-          {
-            key: 'StudentID',
-            sortable: true,
-            class: 'studentid-quizzes'
-          },
-          {
-            key: 'FullName',
-            sortable: true,
-            class: 'fullname-quizzes'
-          },
-          {
-            key: 'Class',
-            sortable: true,
-            class: 'class-quizzes'
-          },
-          {
-            key: 'Actions',
-            sortable: false,
-            class: 'table-checkbox'
-          }
+        selectAll: false,
+        selected: [],
+        optionSelected: 1,
+        options: [
+          { value: 1, text: 'Test Summit 1 Final' },
+          { value: 2, text: 'Test Summit 2 Final' }
         ],
         items: [
           { No: 1, SenderID: 123456789, StudentID: 'GCH12345', FullName: 'Chatbot Edu', Class: 'GCH0717' },
@@ -110,6 +112,28 @@ export default {
           { No: 12, SenderID: 123456789, StudentID: 'GCH88888', FullName: 'Chatbot Edu', Class: 'GCH0717' },
           { No: 13, SenderID: 123456789, StudentID: 'GCH11111', FullName: 'Chatbot Edu', Class: 'GCH0717' },
         ]
+      }
+    },
+    methods: {
+      select() {
+        this.selected = [];
+        if (!this.selectAll) {
+          for (let i in this.items) {
+            this.selected.push(this.items[i].No);
+          }
+        }
+        console.log(this.selected)
+      }
+    },
+    computed: {
+      resultQuery() {
+        if(this.keyword) {
+          return this.items.filter((item) => {
+            return this.keyword.toLowerCase().split(' ').every(v => item.StudentID.toLowerCase().includes(v))
+          })
+        }else {
+          return this.items
+        }
       }
     }
 }
